fa018b1f2e12fa8bd68313d8edd23cfc79639bf9
Mahfuz <mahfuzshishir2811@gmail.com>
2021-10-31 10:31:32 +0600

    adding blade for report showing and filtering

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+    <title>Data Filter</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{ url("css/style.css") }}">
+</head>
+<body>
+
+<div class="container">
+    <div style="margin-top: 10px;" class="row">
+        @yield('content')
+    </div>
+</div>
+
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="col-md-6">
+        <form action="{{ route('index') }}" method="GET">
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="title" class="control-label">Birth Year</label>
+                    <input type="text" class="form-control" name="yob"
+                           value="{{ $yob }}" autocomplete="off">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="title" class="control-label">Birth Month</label>
+                    <input type="text" class="form-control" name="mob"
+                           value="{{ $mob }}" autocomplete="off">
+                </div>
+                <div class="form-group col-md-2">
+                    <button style="margin-top: 52px;" class="btn btn-warning" type="submit">Filter</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="col-md-6">
+        <div style="display: flex; margin-top: 55px; float: right;">
+            <a class="btn border-black {{ $page == 1 ? 'disabled' : '' }}"
+               href="{{ route('index', ['yob' => $yob, 'mob' => $mob, 'page' => $page-1]) }}">
+                Previous
+            </a>
+            <div style="margin: 5px 15px;">{{ (($page-1)*20)+1 }} - {{ ($page*20) > $totalData ? $totalData : ($page*20) }} 0f {{ $totalData }}</div>
+            <a class="btn border-black pd-custom {{ ($page*20) >= $totalData ? 'disabled' : '' }}"
+               href="{{ route('index', ['yob' => $yob, 'mob' => $mob, 'page' => $page+1]) }}">
+                Next
+            </a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-body no-padding table-responsive">
+            <table class="table table-condensed">
+                <thead>
+                <tr class="header">
+                    <th>#</th>
+                    <th>Email Address</th>
+                    <th>Name</th>
+                    <th>DOB</th>
+                    <th>IP</th>
+                    <th>Country</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $id = 1; ?>
+                @foreach($data as $item)
+                <tr class="border-bottom">
+                    <td>{{ $id++ }}</td>
+                    <td>{{ $item['email'] }}</td>
+                    <td>{{ $item['name'] }}</td>
+                    <td>{{ $item['dob'] }}</td>
+                    <td>{{ $item['ip'] }}</td>
+                    <td>{{ $item['country'] }}</td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-12">
+            <div style="display: flex; float: right;">
+                <a class="btn border-black {{ $page == 1 ? 'disabled' : '' }}"
+                   href="{{ route('index', ['yob' => $yob, 'mob' => $mob, 'page' => $page-1]) }}">
+                    Previous
+                </a>
+                <div style="margin: 5px 15px;">{{ (($page-1)*20)+1 }} - {{ ($page*20) > $totalData ? $totalData : ($page*20) }} 0f {{ $totalData }}</div>
+                <a class="btn border-black pd-custom {{ ($page*20) >= $totalData ? 'disabled' : '' }}"
+                   href="{{ route('index', ['yob' => $yob, 'mob' => $mob, 'page' => $page+1]) }}">
+                    Next
+                </a>
+            </div>
+        </div>
+    </div>
+@stop
